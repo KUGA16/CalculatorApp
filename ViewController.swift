f1b7f0a38339376bf6eaf76ba4d6a0b11a1ad8f8
KUGA <kuga@KUGAnoMacBook-Pro.local>
2020-07-22 11:41:23 +0900

    First commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by KUGA on 2020/07/22.
+//  Copyright © 2020 kuga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
